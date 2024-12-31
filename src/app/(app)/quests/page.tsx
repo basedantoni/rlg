04037796599498d19ce48dd2e3afd54c8f395337
Anthony Mercado <antho@antho.local>
2024-12-31 01:22:49 -0600

    create example kanban with roadmap ui

---
+++ src/app/(app)/quests/page.tsx
@@ -0,0 +1,10 @@
+import { Kanban } from "@/components/kanban";
+
+export default function QuestsPage() {
+        return (
+                <>
+                        <h1>Quests</h1>
+                        <Kanban />
+                </>
+        )
+}
